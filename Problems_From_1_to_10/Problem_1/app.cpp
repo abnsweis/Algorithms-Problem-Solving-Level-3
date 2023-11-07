4019171f44847817e3c0f4e7f05a00bc4c9833a2
ibrahim al sweis <abn.sweis@gmail.com>
2023-11-07 18:30:39 +0300

    Add Problem Number 1

---
+++ Problems_From_1_to_10/Problem_1/app.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+
+using namespace std;
+
+int GetRandomNumber(int from, int to) {
+  return rand() % (to - from + 1) + from;
+}
+
+
+
+void FullArrayWithRandomNumbers(short arr_numbers[3][3],short cols,short rows){
+    
+    for (int i = 0; i < cols; i++) {
+        
+        for (int j = 0; j < rows; j++) {
+            arr_numbers[i][j] = GetRandomNumber(1,100);
+        }
+        
+    }
+
+    
+}
+
+
+void PrintMatrexArray(short arr_numbers[3][3],short cols,short rows){
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            cout << arr_numbers[i][j]<< "\t";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    srand((unsigned)time(NULL));
+    short arr_numbers[3][3] ;
+    
+    FullArrayWithRandomNumbers(arr_numbers,3,3);
+    PrintMatrexArray(arr_numbers,3,3);
+    
+    return 0;
+}
