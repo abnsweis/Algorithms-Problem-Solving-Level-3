b94ec6953e9f795e1226fe1d8e4b67e7581cec51
ibrahim al sweis <abn.sweis@gmail.com>
2023-11-12 23:41:46 +0300

    Add Problem Number 6

---
+++ Problems_From_1_to_10/Problem_6/app.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void FillArrayWithOrderdNumbers(int array_numbers[3][3],int cols,int rows){
+    short counter=0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            counter++;
+            array_numbers[i][j]=counter;
+        }
+    }
+    
+} 
+
+
+
+void PrintMatrex(int arr_numbers[3][3], short cols, short rows) {
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            cout << arr_numbers[i][j]<< "\t";
+        }
+        cout << endl;
+    }
+}
+
+
+int main(){
+    int array_of_ordered_numbers[3][3];
+    
+    
+    FillArrayWithOrderdNumbers(array_of_ordered_numbers,3,3);
+    
+    PrintMatrex(array_of_ordered_numbers,3,3);
+    return 0;
+};
