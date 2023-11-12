2284c5377dc770dd6a78a136ff34c9421f87345a
ibrahim al sweis <abn.sweis@gmail.com>
2023-11-13 00:52:42 +0300

    Add Problem Number 1

--- Problems_From_1_to_10/Problem_1/app.cpp
+++ Problems_From_1_to_10/Problem_1/app.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
+#include<string>
+#include<iomanip>
 
 
 using namespace std;
 
 int GetRandomNumber(int from, int to) {
-  return rand() % (to - from + 1) + from;
+    return rand() % (to - from + 1) + from;
 }
 
 
 
-void FullArrayWithRandomNumbers(short arr_numbers[3][3],short cols,short rows){
-    
+void FullArrayWithRandomNumbers(short arr_numbers[3][3], short cols, short rows) {
+
     for (int i = 0; i < cols; i++) {
-        
+
         for (int j = 0; j < rows; j++) {
-            arr_numbers[i][j] = GetRandomNumber(1,100);
+            arr_numbers[i][j] = GetRandomNumber(1, 100);
         }
-        
+
     }
 
-    
+
 }
 
 
-void PrintMatrexArray(short arr_numbers[3][3],short cols,short rows){
+void PrintMatrexArray(short arr_numbers[3][3], short cols, short rows) {
     for (int i = 0; i < cols; i++) {
         for (int j = 0; j < rows; j++) {
             cout << arr_numbers[i][j]<< "\t";
@@ -32,12 +34,12 @@ void PrintMatrexArray(short arr_numbers[3][3],short cols,short rows){
     }
 }
 
-int main(){
+int main() {
     srand((unsigned)time(NULL));
-    short arr_numbers[3][3] ;
-    
-    FullArrayWithRandomNumbers(arr_numbers,3,3);
-    PrintMatrexArray(arr_numbers,3,3);
-    
+    short arr_numbers[3][3];
+    cout << endl << endl << endl << "The Foilowing is a 3 * 3 Ramdom Matrex :" << endl;
+    FullArrayWithRandomNumbers(arr_numbers, 3, 3);
+    PrintMatrexArray(arr_numbers, 3, 3);
+    cout << endl;
     return 0;
 }
