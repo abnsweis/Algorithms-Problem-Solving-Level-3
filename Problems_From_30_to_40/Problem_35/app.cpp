012fd60021e1db6d98ad6326409cc19e885b4537
ibrahim al sweis <abn.sweis@gmail.com>
2024-01-04 15:25:27 +0300

    Add Problem Number 35

---
+++ Problems_From_30_to_40/Problem_35/app.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+string ReadString(){
+	string str="";
+	cout << "Please Enter Your String : ";
+	getline(cin,str);
+	
+	return str;
+}
+
+
+
+void PrintEachWordInString(string str){
+
+	string delimiter = " ";
+	short pos = 0;
+	string Sword =  "";
+	
+	while((pos = str.find(delimiter)) != std::string::npos){
+		
+		Sword = str.substr(0,pos);
+		if(Sword != ""){
+			cout << Sword << endl;	
+		}
+		
+		str.erase(0,pos + delimiter.length());
+	}
+	
+	if(Sword != ""){
+		cout << str << endl;
+	}
+	
+	
+}
+
+int main(){
+	string S1 = ReadString();
+	
+	PrintEachWordInString(S1) ;
+	return 0;
+};
