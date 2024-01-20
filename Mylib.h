b9f21100c67ab14a8bb2a9335f3d74fc64c0672b
ibrahim al sweis <abn.sweis@gmail.com>
2024-01-20 23:36:49 +0300

    Add Lib

--- Mylib.h
+++ Mylib.h
@@ -20,6 +20,34 @@ namespace Mylib{
 	return str;
 } 
 	
+
+	vector <string> split(string str,string delimiter){
+	
+	vector <string> vString;
+	
+	short pos=0;
+	string word = "";
+	
+	
+	while((pos = str.find(delimiter)) != std::string::npos){
+		
+		word = str.substr(0,pos);
+		
+		if(word != ""){
+			vString.push_back(word);
+		}
+		
+		str = str.erase(0,pos + delimiter.length());
+		
+	}
+	
+	if(word != ""){
+		vString.push_back(str);
+	}
+	
+	
+	return vString;
+}
 	
 	
 }
